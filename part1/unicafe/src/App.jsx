b2c493e2f0b1a51702e41fe07f682def783b1a89
Jesse <jesse.heikinheimo@gmail.com>
2024-07-14 19:59:49 +0300

    Part 1 exercise 8 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,6 +14,47 @@ const Button = (props) => {
   )
 }
 
+const Statistics = ({good, bad, neutral, allClicks}) => {
+  const countAverage = (allClicks) => {
+    if (allClicks.length === 0) {
+      return 0;
+    };
+    const sum = allClicks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    console.log("array is", allClicks)
+    console.log("sum", sum)
+    console.log("length", allClicks.length)
+    return sum / allClicks.length;
+  };
+
+  const countPositive = (allClicks) => {
+    if (allClicks.length === 0) {
+      return 0;
+    } else {
+      let counter = 0;
+      allClicks.forEach(element => {
+        if (element === 1) {
+          counter++;
+        }
+      });
+      return (counter / allClicks.length) * 100; 
+    }
+  };
+  
+  const countAll = () => allClicks.length;
+
+  return (
+    <div>
+    <h1>Statistics</h1>
+      <StatElement statText="Good" metric={good}/>
+      <StatElement statText="Neutral" metric={neutral}/>
+      <StatElement statText="Bad" metric={bad}/>
+      <StatElement statText="All" metric={countAll(allClicks)} />
+      <StatElement statText="Average" metric={countAverage(allClicks)} />
+      <StatElement statText="Positive" metric={countPositive(allClicks) + " %"} />
+    </div>
+  )
+}
+
 const StatElement = ({statText, metric}) => {
   return (
     <p>{statText}: {metric}</p>
@@ -21,7 +62,6 @@ const StatElement = ({statText, metric}) => {
 }
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -45,46 +85,13 @@ const App = () => {
     setBad(bad + 1)
   }
 
-  const countAverage = (allClicks) => {
-    if (allClicks.length === 0) {
-      return 0;
-    }
-    const sum = allClicks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    console.log("array is", allClicks)
-    console.log("sum", sum)
-    console.log("length", allClicks.length)
-    return sum / allClicks.length;
-  };
-
-  const countPositive = (allClicks) => {
-    if (allClicks.length === 0) {
-      return 0;
-    } else {
-      let counter = 0;
-      allClicks.forEach(element => {
-        if (element === 1) {
-          counter++;
-        }
-      });
-      return (counter / allClicks.length) * 100; 
-    }
-  }
-  
-  const countAll = () => allClicks.length 
-
   return (
     <div>
       <Header text="Give feedback" />
       <Button handleCLick={handleGoodClick} text='Good' />  
       <Button handleCLick={handleNeutralClick} text='Neutral' />  
       <Button handleCLick={handleBadClick} text='Bad' />  
-      <h1>Statistics</h1>
-      <StatElement statText="Good" metric={good}/>
-      <StatElement statText="Neutral" metric={neutral}/>
-      <StatElement statText="Bad" metric={bad}/>
-      <StatElement statText="All" metric={countAll(allClicks)} />
-      <StatElement statText="Average" metric={countAverage(allClicks)} />
-      <StatElement statText="Positive" metric={countPositive(allClicks) + " %"} />
+      <Statistics good={good} bad={bad} neutral={neutral} allClicks={allClicks} />
     </div>
   )
 }
