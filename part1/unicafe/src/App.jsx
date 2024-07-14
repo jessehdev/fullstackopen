706be6b6ef37621a7b39ba39df9a848f923a41ab
Jesse <jesse.heikinheimo@gmail.com>
2024-07-14 19:49:46 +0300

    Part1 exercise 7 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,9 +14,9 @@ const Button = (props) => {
   )
 }
 
-const StatElement = ({statText, metric, allClicks}) => {
+const StatElement = ({statText, metric}) => {
   return (
-    <p>{statText}: {allClicks.filter(r => r === metric).length}</p>
+    <p>{statText}: {metric}</p>
   )
 }
 
@@ -29,21 +29,48 @@ const App = () => {
 
   const handleGoodClick = () => {
     console.log("Good clicked")
-    setAll(allClicks.concat('G'))
+    setAll(allClicks.concat(1))
     setGood(good + 1)
   }
 
   const handleNeutralClick = () => {
     console.log("Neutal clicked")
-    setAll(allClicks.concat('N'))
+    setAll(allClicks.concat(0))
     setNeutral(neutral + 1)
   }
 
   const handleBadClick = () => {
     console.log("BAd clicked")
-    setAll(allClicks.concat('B'))
+    setAll(allClicks.concat(-1))
     setBad(bad + 1)
   }
+
+  const countAverage = (allClicks) => {
+    if (allClicks.length === 0) {
+      return 0;
+    }
+    const sum = allClicks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    console.log("array is", allClicks)
+    console.log("sum", sum)
+    console.log("length", allClicks.length)
+    return sum / allClicks.length;
+  };
+
+  const countPositive = (allClicks) => {
+    if (allClicks.length === 0) {
+      return 0;
+    } else {
+      let counter = 0;
+      allClicks.forEach(element => {
+        if (element === 1) {
+          counter++;
+        }
+      });
+      return (counter / allClicks.length) * 100; 
+    }
+  }
+  
+  const countAll = () => allClicks.length 
 
   return (
     <div>
@@ -52,9 +79,12 @@ const App = () => {
       <Button handleCLick={handleNeutralClick} text='Neutral' />  
       <Button handleCLick={handleBadClick} text='Bad' />  
       <h1>Statistics</h1>
-      <StatElement statText="Good" metric="G" allClicks={allClicks}/>
-      <StatElement statText="Neutal" metric="N" allClicks={allClicks}/>
-      <StatElement statText="Bad" metric="B" allClicks={allClicks}/>
+      <StatElement statText="Good" metric={good}/>
+      <StatElement statText="Neutral" metric={neutral}/>
+      <StatElement statText="Bad" metric={bad}/>
+      <StatElement statText="All" metric={countAll(allClicks)} />
+      <StatElement statText="Average" metric={countAverage(allClicks)} />
+      <StatElement statText="Positive" metric={countPositive(allClicks) + " %"} />
     </div>
   )
 }
